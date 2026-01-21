d5c902d83084b30ab3c8d43bdd0c6cf9743d98bf
fernandomacias077z-ui <fernandomacias077zgmail.com>
2026-01-20 23:30:20 -0500

    ahorasi

--- tasks/views.py
+++ tasks/views.py
@@ -1,30 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_4axis_shortcut, get_object_or_404
+# Línea corregida (cambiamos '4axis' por '404')
+from django.shortcuts import render, redirect, get_object_or_404 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from .models import Task  # Solo importamos Task
+from .models import Task
 from django.utils import timezone
 
-def home(request):
-    return render(request, 'home.html')
-
-def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html', {'form': UserCreationForm()})
-    else:
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('tasks')
-        else:
-            return render(request, 'signup.html', {'form': form})
-
-@login_required
-def tasks(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'tasks.html', {'tasks': tasks})
-
-# Si tienes más funciones abajo (create_task, detail, etc.), 
-# asegúrate de que ninguna use "Experience" o "DatosPersonales"
+# ... resto de tus funciones (home, signup, tasks, etc.)
