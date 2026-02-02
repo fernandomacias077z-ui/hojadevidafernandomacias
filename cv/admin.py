82cb5fb1ed4b0ecda514470f8427ccd39e47dc8f
Jean Zambrano <jeanpierrezambranodatos@gmail.com>
2026-02-02 11:25:10 -0500

    Actualización: Multiples perfiles, PDF con foto y Cursos

--- cv/admin.py
+++ cv/admin.py
@@ -1,23 +1,45 @@
 from django.contrib import admin
 from .models import Perfil, Experiencia, Educacion, Proyecto, Certificado, Producto, Curso
 
-# --- ESTO ES LO QUE CAMBIA EL TEXTO ---
-# Al ponerlo aquí, nos aseguramos de que se aplique al cargar la app
-admin.site.site_header = "Admin"             # Texto grande en la barra azul
-admin.site.site_title = "Admin"              # Título en la pestaña del navegador
-admin.site.index_title = "Panel de Control"  # Subtítulo (opcional)
+# --- PERSONALIZACIÓN DEL ADMIN ---
+admin.site.site_header = "Admin"
+admin.site.site_title = "Panel de Admin"
+admin.site.index_title = "Panel de Control"
 
-# --- TUS REGISTROS ---
-admin.site.register(Perfil)
-admin.site.register(Experiencia)
-admin.site.register(Educacion)
-admin.site.register(Proyecto)
-admin.site.register(Certificado)
-admin.site.register(Producto)
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'dni', 'profesion')
+    search_fields = ('nombre', 'apellido', 'dni')
+
+@admin.register(Experiencia)
+class ExperienciaAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'empresa', 'perfil', 'fecha_inicio')
+    list_filter = ('perfil',) # Filtro lateral para ver experiencias por perfil
+
+@admin.register(Educacion)
+class EducacionAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'institucion', 'perfil', 'fecha')
+    list_filter = ('perfil',)
+
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'perfil', 'fecha')
+    list_filter = ('perfil',)
+
+@admin.register(Certificado)
+class CertificadoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'institucion', 'perfil', 'fecha')
+    list_filter = ('perfil',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'precio', 'estado', 'perfil', 'disponible')
+    list_filter = ('perfil', 'estado')
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'fecha', 'ver_pdf')
+    list_display = ('titulo', 'fecha', 'perfil', 'ver_pdf')
+    list_filter = ('perfil',)
     search_fields = ('titulo', 'descripcion')
     
     def ver_pdf(self, obj):
