1e9104e018eb075e3081b41005b11b30c2d493bd
fernandomacias077z-ui <fernandomacias077zgmail.com>
2026-02-03 12:19:34 -0500

    Fix: Agregado modelo Certificado para corregir error de importación

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Instalar librerías
 pip install -r requirements.txt
-
-# RECOLECTAR ESTÁTICOS (ESTO ARREGLA EL ADMIN EN RENDER)
-python manage.py collectstatic --no-input
-
-# Aplicar cambios en la base de datos
+python manage.py collectstatic --no-input # <--- ESTO ARREGLA EL DISEÑO
 python manage.py migrate
